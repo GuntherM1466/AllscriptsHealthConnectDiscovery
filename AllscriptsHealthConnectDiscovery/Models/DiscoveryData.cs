cf0ce2ce0d31363744ee41693b12dabf0595f499
GUNTHERM <Gunther.Meyer@allscripts.com>
2017-05-04 10:44:39 -0400

    Added a missing file

---
+++ AllscriptsHealthConnectDiscovery/Models/DiscoveryData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestPatientApp.Models
+{
+    public class DiscoveryData
+    {
+        public string html_encoded_json_disocvery_data { get; set; }
+    }
+}
